6d59415b8c2ec8e3815e7051917d30064c910026
Hannah <Hannah@LAPTOP-5M1SQJLA>
2021-08-01 22:29:18 +0800

    Created Account Class

--- C206_CaseStudy/src/Account.java
+++ C206_CaseStudy/src/Account.java
@@ -9,4 +9,19 @@
 public class Account {
 	private String username;
 	private String password;
+	
+	public Account(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
 }
