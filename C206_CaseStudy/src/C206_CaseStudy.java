3928ffa5ed77648ae7faec616cf56ccd3a720be2
User <User@DESKTOP-J8QI278>
2021-08-03 00:05:49 +0800

    Format

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,7 +2,8 @@
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
-
+	
+	private static final int OPTION_QUIT = 6;
 	public static void main(String[] args) {
 
 		ArrayList<western> westernArr = new ArrayList<western>();
@@ -15,10 +16,10 @@ public class C206_CaseStudy {
 		westernArr.add(new western("Spaghetti"));
 		westernArr.add(new western("Fish & Chips"));
 
-		asianArr.add(new asian("chilli crab"));
-		asianArr.add(new asian("chicken rice"));
-		asianArr.add(new asian("nasi lemak"));
-		asianArr.add(new asian("cai fan"));
+		asianArr.add(new asian("Chilli Crab"));
+		asianArr.add(new asian("Chicken Rice"));
+		asianArr.add(new asian("Nasi Lemak"));
+		asianArr.add(new asian("Cai Fan"));
 
 		drinksArr.add(new drinks("Coke"));
 		drinksArr.add(new drinks("Fanta"));
@@ -30,131 +31,227 @@ public class C206_CaseStudy {
 		fruitsArr.add(new Fruits("WaterMelon"));
 		fruitsArr.add(new Fruits("BlueBerry"));
 
-		menu();
-
-		int choice = Helper.readInt("Enter choice: ");
-
-		while (choice != 4) {
-			if (choice == 1) {
-				for (western i : westernArr) {
-					System.out.println("Western Food: " + i.getName());
-				}
-				System.out.println("");
-
-				for (asian i : asianArr) {
-					System.out.println("Asian Food: " + i.getName());
-				}
-				System.out.println("");
-
-				for (drinks i : drinksArr) {
-					System.out.println("Drinks: " + i.getName());
-				}
-				System.out.println("");
-
-				for (Fruits i : fruitsArr) {
-					System.out.println("Fruits: " + i.getName());
-				}
-				System.out.println("");
-
-				choice = Helper.readInt("Enter choice: ");
-			} else if (choice == 2) {
-
-				String category = Helper.readString("Choose a category to delete from: ");
-
-				if (category.equalsIgnoreCase("western")) {
-					String item = Helper.readString("Choose a item to delete: ");
+		
+		int option = 0;
+		
+		while (option != OPTION_QUIT) {
+			
+			menu();
+			option = Helper.readInt("Enter option: ");
+			
+			if(option == 1) {
+				// Hannah User Account 
+				
+			}else if (option == 2) {
+				// Kidson Menu Items
+				
+				itemMenu();
+				
+				int itemOption = Helper.readInt("Enter option: ");
+				
+				if (itemOption == 1) {
 					for (western i : westernArr) {
-						if (item.equals(i.getName())) {
-							i = null;
-						
-						}
+						System.out.println("Western Food: " + i.getName());
 					}
+					System.out.println("");
 
-				} else if (category.equalsIgnoreCase("asian")) {
 					for (asian i : asianArr) {
+						System.out.println("Asian Food: " + i.getName());
+					}
+					System.out.println("");
+
+					for (drinks i : drinksArr) {
+						System.out.println("Drinks: " + i.getName());
+					}
+					System.out.println("");
+
+					for (Fruits i : fruitsArr) {
+						System.out.println("Fruits: " + i.getName());
+					}
+					System.out.println("");
+
+				} else if (itemOption == 2) {
+
+					String category = Helper.readString("Choose a category to delete from: ");
+
+					if (category.equalsIgnoreCase("western")) {
 						String item = Helper.readString("Choose a item to delete: ");
-						if (item.equals(i.getName())) {
-							i = null;
+						for (western i : westernArr) {
+							if (item.equals(i.getName())) {
+								i = null;
 							
+							}
 						}
-					}
-				} else if (category.equalsIgnoreCase("drinks")) {
 
-					for (drinks i : drinksArr) {
-						String item = Helper.readString("Choose a item to delete: ");
-						if (item.equals(i.getName())) {
-							i = null;
-						
+					} else if (category.equalsIgnoreCase("asian")) {
+						for (asian i : asianArr) {
+							String item = Helper.readString("Choose a item to delete: ");
+							if (item.equals(i.getName())) {
+								i = null;
+								
+							}
+						}
+					} else if (category.equalsIgnoreCase("drinks")) {
+
+						for (drinks i : drinksArr) {
+							String item = Helper.readString("Choose a item to delete: ");
+							if (item.equals(i.getName())) {
+								i = null;
+							
+							}
+						}
+					} else if (category.equalsIgnoreCase("fruits")) {
+						for (Fruits i : fruitsArr) {
+							String item = Helper.readString("Choose a item to delete: ");
+							if (item.equals(i.getName())) {
+								i = null;
+							
+							}
 						}
 					}
-				} else if (category.equalsIgnoreCase("fruits")) {
-					for (Fruits i : fruitsArr) {
-						String item = Helper.readString("Choose a item to delete: ");
-						if (item.equals(i.getName())) {
-							i = null;
-						
+					
+
+				} else if (itemOption == 3) {
+					String category = Helper.readString("Choose a category to add to: ");
+
+					if (category.equalsIgnoreCase("western")) {
+						String item = Helper.readString("Choose a item to add: ");
+						for (western i : westernArr) {
+							if (i == null) {
+								i = new western(item);
+								westernArr.add(i);
+
+							}
 						}
+
+					} else if (category.equalsIgnoreCase("asian")) {
+						for (asian i : asianArr) {
+							String item = Helper.readString("Choose a item to add: ");
+							if (i == null) {
+								i = new asian(item);
+								asianArr.add(i);
+
+							}
+						}
+					} else if (category.equalsIgnoreCase("drinks")) {
+
+						for (drinks i : drinksArr) {
+							String item = Helper.readString("Choose a item to add: ");
+							if (i == null) {
+								i = new drinks(item);
+								drinksArr.add(i);
+
+							}
+						}
+					} else if (category.equalsIgnoreCase("fruits")) {
+						for (Fruits i : fruitsArr) {
+							String item = Helper.readString("Choose a item to add: ");
+							if (i == null) {
+								i = new Fruits(item);
+								fruitsArr.add(i);
+							}
+						}
+
+					}else {
+						System.out.println("Invalid option");
 					}
+				
 				}
-				choice = Helper.readInt("Enter choice: ");
-
-			} else if (choice == 3) {
-				String category = Helper.readString("Choose a category to add to: ");
-
-				if (category.equalsIgnoreCase("western")) {
-					String item = Helper.readString("Choose a item to add: ");
-					for (western i : westernArr) {
-						if (i == null) {
-							i = new western(item);
-							westernArr.add(i);
-
-						}
-					}
-
-				} else if (category.equalsIgnoreCase("asian")) {
-					for (asian i : asianArr) {
-						String item = Helper.readString("Choose a item to add: ");
-						if (i == null) {
-							i = new asian(item);
-							asianArr.add(i);
-
-						}
-					}
-				} else if (category.equalsIgnoreCase("drinks")) {
-
-					for (drinks i : drinksArr) {
-						String item = Helper.readString("Choose a item to add: ");
-						if (i == null) {
-							i = new drinks(item);
-							drinksArr.add(i);
-
-						}
-					}
-				} else if (category.equalsIgnoreCase("fruits")) {
-					for (Fruits i : fruitsArr) {
-						String item = Helper.readString("Choose a item to add: ");
-						if (i == null) {
-							i = new Fruits(item);
-							fruitsArr.add(i);
-						}
-					}
-
-				}
-				choice = Helper.readInt("Enter choice: ");
+				
+			}else if (option == 3) {
+				//Ying Teng Monthly Menu
+				
+				
+				
+			}else if (option == 4) {
+				// Angela Lunch Box Order
+				
+				
+				
+				
+			}else if (option == 5) {
+				// Syazwan Bills 
+				
+				
+				
+			}else if (option == OPTION_QUIT) {
+				System.out.println("Bye!");
+			}else {
+				System.out.println("Invalid option");
 			}
 		}
 
 	}
 
-	// TODO Auto-generated method stub
 
+	
+	
 	private static void menu() {
-		// TODO Auto-generated method stub
+		C206_CaseStudy.setHeader("LUNCH BOX ORDERING APP");
+		System.out.println("1. User Account");
+		System.out.println("2. Menu Items");
+		System.out.println("3. Monthly Menu");
+		System.out.println("4. Lunch Box Order");
+		System.out.println("5. Bill");
+		System.out.println("6. Quit");
+		Helper.line(80, "-");
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+
+	private static void itemMenu() {
 		System.out.println("1: Show all Inside Menu Bank");
 		System.out.println("2: Remove a item from menu bank");
 		System.out.println("3: Add a item into menu bank");
-		System.out.println("4: Quit");
 
 	}
+	
+	//================================================ OPTION 1 USER ACCOUNT  ================================================
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//=============================================== OPTION 2 MENU ITEMS ==================================================
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//=============================================== OPTION 3 MONTHLY MENU ==================================================
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//=============================================== OPTION 4 LUNCH BOX ORDER ==================================================
+	
+	
+	
+	
+	
+	
+	//=============================================== OPTION 5 BILL ==================================================
+	
+	
+	
+	
+	
 
 }
