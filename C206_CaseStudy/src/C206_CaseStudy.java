ddf1beabf4f0f5fabca086d06cbd4fe0b93362b7
20009188 <20009188@LAPTOP-TFVVA551>
2021-08-06 18:13:30 +0800

    Alignment

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -311,8 +311,7 @@ public class C206_CaseStudy {
 		System.out.println("3. Delete Order Bill");
 	}
 
-	// ================================================ OPTION 1 USER ACCOUNT
-	// ================================================
+	//================================================= OPTION 1 USER ACCOUNT ============================================//
 
 	// Add user account
 	public static Account inputAccount() {
@@ -378,8 +377,7 @@ public class C206_CaseStudy {
 		}
 	}
 
-	// =============================================== OPTION 2 MENU ITEMS
-	// ==================================================
+	//================================================ OPTION 2 MENU ITEMS ===============================================//
 	// View items From menu bank
 	public static String viewWestern(ArrayList<western> westernArr) {
 		String output = "";
@@ -533,8 +531,7 @@ public class C206_CaseStudy {
 
 	}
 
-	// =============================================== OPTION 3 MONTHLY MENU
-	// ==================================================
+	//================================================ OPTION 3 MONTHLY MENU =============================================//
 
 	public static String randomWestern() {
 		Random rand = new Random();
@@ -661,8 +658,7 @@ public class C206_CaseStudy {
 		return output;
 	}
 
-	// =============================================== OPTION 4 LUNCH BOX ORDER
-	// ==================================================
+	//================================================ OPTION 4 LUNCH BOX ORDER ==========================================//
 	public static LunchBox inputLunchBox() {
 
 		int orderID = Helper.readInt("Enter order ID > ");
@@ -730,8 +726,7 @@ public class C206_CaseStudy {
 		}
 	}
 
-	// =============================================== OPTION 5 BILL
-	// ==================================================
+	//================================================ OPTION 5 BILL =====================================================//
 	public static void createOrderBill(ArrayList<Bill> orderBillArr) {
 		String username = Helper.readString("Enter username > ");
 		String menu = Helper.readString("Enter menu category > ");
